b5875d92a25f67f2fba55be7be6e69179ee8d067
umi <9@umi.cat>
2024-10-22 16:15:33 +0800

    add speedinsights

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import TrendingCoins from './components/TrendingCoins';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 export default function Home() {
   return (
     <>
       <TrendingCoins />
       <Analytics />
+      <SpeedInsights />
     </>
   );
 }
